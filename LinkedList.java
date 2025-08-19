d990c2c19636b7da9550556053399be168c28188
Purnapranitha <gopireddypranitha13@gmail.com>
2025-08-19 03:28:21 -0700

    Add files via upload

---
+++ LinkedList.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+public class Linkedlist {
+    public static void main(String []args){
+        LinkedList<Integer>ll=new LinkedList<>();
+           ll.add(10);
+           ll.add(9);
+           ll.add(13);
+           ll.add(32);
+           System.out.println(ll);
+    }
+}
